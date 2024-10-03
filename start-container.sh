2a98bbef6f8f7813e958e2341a9a65cf8b035c28
NorDar <44843808+NorDar@users.noreply.github.com>
2024-10-03 12:49:44 +0200

    extend start-container.sh

--- start-container.sh
+++ start-container.sh
@@ -1,30 +1,29 @@
+#!/bin/bash
 usedPorts=$(netstat -latn --inet | awk '{print $4}' | awk -F : '{print $2}' | sort -u)
-for port in {8888..9999}; do
+for port in {8988..8999}; do
   echo "trying port $port"
   echo "$usedPorts" | grep -q "^$port$" || break
 done
 echo "starting container with jupyter notebook on port $port"
 
-
-docker run -ti -d \
-        --name skull-strip-gpu \
-        -u $(id -u):$(id -g) \
+docker run -d \
+        --name img-pipeline-dari \
+        -u "$(id -u)":"$(id -g)" \
         -v /home/dari:/home/dari \
         -v /home:/host-homes \
-        -v "/etc/group:/etc/group:ro" \
-        -v "/etc/passwd:/etc/passwd:ro" \
-        -v "/etc/shadow:/etc/shadow:ro" \
-        --workdir=/home/dari \
-        -p $port:$port  --gpus device=0 \
-        nordar/skull-strip-pipeline:2.12.0 \
-        bash -c 'export PATH=$PATH:/.local/bin; jupyter notebook --no-browser --ip=0.0.0.0 --port='"$port"' --notebook-dir /home/dari --NotebookApp.token=eee'
+        -v /home:/tf/notebooks \
+        -v /etc/group:/etc/group:ro \
+        -v /etc/passwd:/etc/passwd:ro \
+        -v /etc/shadow:/etc/shadow:ro \
+        -e JUPYTER_TOKEN="eee"\
+        -e JUPYTER_ENABLE_LAB="yes" \
+        -e NB_UID="$(id -u)" \
+        -e NB_GID="$(id -g)" \
+        -e JUPYTER_PORT="$port" \
+        -p "$port":"$port" \
+        --gpus device=0 --workdir=/home/dari \
+        nordar/skull-strip-pipeline:2.17.0-01
 
-# not mapped
-docker run -ti \
-        --name skull-strip-gpu \
-        -p $port:$port \
-        nordar/skull-strip-pipeline:2.12.0 \
-        bash -c 'export PATH=$PATH:/.local/bin; jupyter notebook --allow-root --no-browser --ip=0.0.0.0 --port='"$port"' --NotebookApp.token=eee'
 
 # windows
 docker run -ti `
